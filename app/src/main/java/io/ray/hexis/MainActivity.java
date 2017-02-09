f91f325b04386a986b38599c007b845f53da38dc
AndrewGEvans95 <Andrewgevans95@gmail.com>
2017-02-09 17:48:11 -0500

    Add basic navigation drawer
    Added a basic navigation drawer to activity_main.  Added
    DrawerLayout which includes main_activity layout.  MainAcitiy
    now sets content view to the aforementioned DrawerLayout.
    Created new resource directory menu to hold navigation menu layout.
    Updated dimens file as per Android Design guidelines for navigation
    screen margins.  Added onBackPress override in activity_main
    to handle closing navigation drawer.

--- app/src/main/java/io/ray/hexis/MainActivity.java
+++ app/src/main/java/io/ray/hexis/MainActivity.java
@@ -1,16 +1,24 @@
 package io.ray.hexis;
 
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+
+        // Initialize navigation view object
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         // Initialize view pager object that hand one time matrix
         ViewPager mViewPager = (ViewPager) findViewById(R.id.quadrant_view_pager);
@@ -20,5 +28,34 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.quadrant_tab_layout);
         tabLayout.setupWithViewPager(mViewPager);
 
+    }
+
+    // Close navigation drawer when back button is pressed
+    @Override public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody") @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_add_goal) {
+            // Handle add goal action here
+        } else if (id == R.id.nav_goal1) {
+            // Handle open goal action here
+        } else if (id == R.id.nav_goal2) {
+            // Handle open goal action here
+        }
+
+        // Close the navigation drawer after item is selected
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
