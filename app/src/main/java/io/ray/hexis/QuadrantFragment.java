37a480dff1697e8455c2d710584c8aaede6dc7e2
Austin Ray <austinray1995@gmail.com>
2017-02-09 10:18:29 -0500

    Add Butterknife,RecyclerViews as dependencies
    Added Butterknife and RecyclerView as dependencies, and used Butterknife
    to bind the QuadrantFragment RecyclerView.

--- app/src/main/java/io/ray/hexis/QuadrantFragment.java
+++ app/src/main/java/io/ray/hexis/QuadrantFragment.java
@@ -4,14 +4,22 @@ package io.ray.hexis;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Viewer class that displays one quadrant of a time matrix
  */
 public class QuadrantFragment extends Fragment {
+
+    @BindView(R.id.quadrant_list)
+    RecyclerView quadRecView;
 
     /**
      * Create a new instance of a QuadrantFragment
@@ -23,7 +31,14 @@ public class QuadrantFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_quadrant, container, false);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        // Create the root view by inflating the fragmant_quadrant layout
+        View root = inflater.inflate(R.layout.fragment_quadrant, container, false);
+
+        // Inject the views
+        ButterKnife.bind(root);
+
+        return root;
     }
 }
---
+++ app/src/main/java/io/ray/hexis/QuadrantItem.java
@@ -0,0 +1,8 @@
+package io.ray.hexis;
+
+/**
+ * Created by aray on 2/9/17.
+ */
+
+public class QuadrantItem {
+}
