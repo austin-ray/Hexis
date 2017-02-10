e8f6ff3e7b595f7d730e53a3ad4546b2c62095fa
AndrewGEvans95 <Andrewgevans95@gmail.com>
2017-02-10 16:36:26 -0500

    Add toolbar to replace actionbar
    Replaced ActionBar with toolbar.  Changed
    theme from DarkActionBar to NoActionbar.
    Added navigation drawer icon to toolbar.

--- app/src/main/java/io/ray/hexis/MainActivity.java
+++ app/src/main/java/io/ray/hexis/MainActivity.java
@@ -5,8 +5,10 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -16,9 +18,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize toolbar object
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         // Initialize navigation view object
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // Initialize drawer layout object
+        // This adds the three horizontal navigation drawer bars in the toolbar
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle =
+            new ActionBarDrawerToggle(this, drawer, toolbar, 0, 0);
+        toggle.syncState();
 
         // Initialize view pager object that hand one time matrix
         ViewPager mViewPager = (ViewPager) findViewById(R.id.quadrant_view_pager);
