88252132e6d1d6a8489e9b9dafe4d5f4ba9f7ffd
Austin Ray <austinray1995@gmail.com>
2017-02-11 01:07:04 -0500

    Add unit test for AddItemOnClickListener

--- app/src/test/java/io/ray/hexis/AddItemOnClickListenerTest.java
+++ app/src/test/java/io/ray/hexis/AddItemOnClickListenerTest.java
@@ -1,5 +1,7 @@
 package io.ray.hexis;
 
+import android.support.v4.view.ViewPager;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
@@ -16,6 +18,10 @@ public class AddItemOnClickListenerTest {
         MainActivity activity = Robolectric.setupActivity(MainActivity.class);
 
         activity.findViewById(R.id.floating_action_button).performClick();
+        ViewPager pager = (ViewPager) activity.findViewById(R.id.quadrant_view_pager);
+
+        AddItemOnClickListener listener = new AddItemOnClickListener(pager);
+        listener.onClick(activity.findViewById(R.id.floating_action_button));
     }
 
 }
