e972c1525f9a70a7c765ad8661e5d053d12116d6
Austin Ray <austinray1995@gmail.com>
2017-02-12 01:22:31 -0500

    Add complete testing coverage for QuadrantItem

--- app/src/test/java/io/ray/hexis/QuadrantItemTest.java
+++ app/src/test/java/io/ray/hexis/QuadrantItemTest.java
@@ -13,6 +13,37 @@ import static org.junit.Assert.*;
 @Config(constants = BuildConfig.class, sdk = 22)
 public class QuadrantItemTest {
     @Test
+    public void quadrantCreatorArray() throws Exception {
+        int size = 10;
+        QuadrantItem[] array = QuadrantItem.CREATOR.newArray(size);
+
+        assertEquals(size, array.length);
+    }
+
+    @Test
+    public void createFromParcel() throws Exception {
+        String msg = "TEST";
+
+        // Create a new item with the msg String
+        QuadrantItem item = new QuadrantItem(msg);
+
+        // Obtain a Parcel
+        Parcel src = Parcel.obtain();
+
+        // Save the item to a Parcel
+        item.writeToParcel(src, 0);
+
+        // Reset data position
+        src.setDataPosition(0);
+
+        // Create a new item from the saved Parcel
+        QuadrantItem test = QuadrantItem.CREATOR.createFromParcel(src);
+
+        // Assert that the messages are equal
+        assertEquals(msg, test.getMessage());
+    }
+
+    @Test
     public void describeContents() throws Exception {
         QuadrantItem test = new QuadrantItem("TEST");
         assertEquals(0, test.describeContents());
