1d6271b2cc932d9c8b2d86cf3d19d12ed6a570f3
Austin Ray <austinray1995@gmail.com>
2017-02-11 23:36:31 -0500

    Convert QuadrantItem to Parcelable
    Added the Parcelable interface to the QuadrantItem to allow saving
    the state of a QuadrantFragment

--- app/src/test/java/io/ray/hexis/QuadrantItemTest.java
+++ app/src/test/java/io/ray/hexis/QuadrantItemTest.java
@@ -1,10 +1,35 @@
 package io.ray.hexis;
 
+import android.os.Parcel;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.*;
 
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 22)
 public class QuadrantItemTest {
+    @Test
+    public void describeContents() throws Exception {
+        QuadrantItem test = new QuadrantItem("TEST");
+        assertEquals(0, test.describeContents());
+    }
+
+    @Test
+    public void writeToParcel() throws Exception {
+        QuadrantItem test = new QuadrantItem("TEST");
+
+        Parcel dest = Parcel.obtain();
+        test.writeToParcel(dest, 0);
+        dest.setDataPosition(0);
+
+        assertEquals("TEST", dest.readString());
+    }
+
     @Test
     public void getMessage() throws Exception {
         QuadrantItem test = new QuadrantItem("TEST");
