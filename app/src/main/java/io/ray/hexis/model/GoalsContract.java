059f2fd75d77c480460f22549afd4a2e9ba55a94
Austin Ray <austinray1995@gmail.com>
2017-02-19 23:32:03 -0500

    Add GoalsContract and QuadrantItemsContract
    Added GoalsContract and QuadrantItemsContract to make management of SQL
    tables easier. This allows easy use of table names and columns through
    the use of static values.
    
    All hardcoded references to the tables have been modified to reflect the
    change. Changes to the tables should be significantly easier now.

---
+++ app/src/main/java/io/ray/hexis/model/GoalsContract.java
@@ -0,0 +1,15 @@
+package io.ray.hexis.model;
+
+
+import android.provider.BaseColumns;
+
+public final class GoalsContract {
+    // Prevent instantiation
+    private GoalsContract() { }
+
+    public static class GoalsEntry implements BaseColumns {
+        public static final String TABLE_NAME = "goals";
+        public static final String COLUMN_NAME_ID = "id";
+        public static final String COLUMN_NAME_GOAL_TITLE = "goal_title";
+    }
+}
--- app/src/main/java/io/ray/hexis/model/SQLiteHelper.java
+++ app/src/main/java/io/ray/hexis/model/SQLiteHelper.java
@@ -4,29 +4,42 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import io.ray.hexis.model.GoalsContract.GoalsEntry;
+import io.ray.hexis.model.QuadrantItemsContract.QuadrantItemsEntry;
+
 public class SQLiteHelper extends SQLiteOpenHelper {
 
     /**
      * Database structure variables defined here
      * Dtabase is stored in /data/data/<packagename>/database
      */
-    public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "Hexis.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "Hexis.db";
 
     // Command to create Goals table
-    public static final String SQL_CREATE_GOALS_TABLE = "CREATE TABLE \"Goals\" ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `goal_title` TEXT );";
+    private static final String SQL_CREATE_GOALS_TABLE =
+            "CREATE TABLE " + GoalsEntry.TABLE_NAME + "(" +
+                    GoalsEntry.COLUMN_NAME_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    GoalsEntry.COLUMN_NAME_GOAL_TITLE + " TEXT );";
 
     // Command to create QuadrantItems table
-    public static final String SQL_CREATE_QUADRANTITEMS_TABLE = "CREATE TABLE \"QuadrantItems\" ( `id` INTEGER PRIMARY KEY AUTOINCREMENT, `goal_id` INTEGER, `item_text` TEXT, `quadrant` INTEGER, `completion_status` INTEGER );";
+    private static final String SQL_CREATE_QUADRANT_ITEMS_TABLE =
+            "CREATE TABLE " + QuadrantItemsEntry.TABLE_NAME + " ( " +
+                    QuadrantItemsEntry.COLUMN_NAME_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    QuadrantItemsEntry.COLUMN_NAME_GOAL_ID + " INTEGER," +
+                    QuadrantItemsEntry.COLUMN_NAME_ITEM_TEXT + " TEXT," +
+                    QuadrantItemsEntry.COLUMN_NAME_QUADRANT + " INTEGER, " +
+                    QuadrantItemsEntry.COLUMN_NAME_COMPLETION_STATUS + " INTEGER );";
 
     // Command to delete Goal table
-    public static final String SQL_DELETE_GOALS = "DROP TABLE IF EXISTS Goals";
+    private static final String SQL_DELETE_GOALS = "DROP TABLE IF EXISTS " + GoalsEntry.TABLE_NAME;
 
     // Command to delete QuadrantItems table
-    public static final String SQL_DELETE_QUADRANTITEMS = "DROP TABLE IF EXISTS QuadrantItems";
+    private static final String SQL_DELETE_QUADRANT_ITEMS = "DROP TABLE IF EXISTS " +
+            QuadrantItemsEntry.TABLE_NAME;
 
     // Purge database variable, set to true if database needs to be reset
-    public static final boolean PURGE_DATABASE = false;
+    private static final boolean PURGE_DATABASE = false;
 
     /**
      * Constructor used to create database if database does not already exist
@@ -34,7 +47,7 @@ public class SQLiteHelper extends SQLiteOpenHelper {
      */
     public SQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        if(PURGE_DATABASE) {
+        if (PURGE_DATABASE) {
             //For testing purposes we need to delete the database so onCreate will be called
             context.deleteDatabase(DATABASE_NAME);
             context.deleteFile(DATABASE_NAME);
@@ -43,26 +56,32 @@ public class SQLiteHelper extends SQLiteOpenHelper {
 
     /**
      * This method is only called if the database does not already exist
+     *
      * @param db
      */
-    @Override public void onCreate(SQLiteDatabase db) {
-        // Create Goals and QuadrantItems tables in Hexis database if the database does not already exist
-            db.execSQL(SQL_CREATE_GOALS_TABLE);
-            db.execSQL(SQL_CREATE_QUADRANTITEMS_TABLE);
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Create Goals and QuadrantItems tables in Hexis database if the database
+        // does not already exist
+        db.execSQL(SQL_CREATE_GOALS_TABLE);
+        db.execSQL(SQL_CREATE_QUADRANT_ITEMS_TABLE);
     }
 
     /**
      * This method is only called if DATABASE_VERSION is changed
+     *
      * @param db
      * @param oldVersion
      * @param newVersion
      */
-    @Override public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // Delete Goals and QuadrantItems tables in Hexis database
         db.execSQL(SQL_DELETE_GOALS);
-        db.execSQL(SQL_DELETE_QUADRANTITEMS);
+        db.execSQL(SQL_DELETE_QUADRANT_ITEMS);
+
         // Do what is necessary to update Hexis database
         db.execSQL(SQL_CREATE_GOALS_TABLE);
-        db.execSQL(SQL_CREATE_QUADRANTITEMS_TABLE);
+        db.execSQL(SQL_CREATE_QUADRANT_ITEMS_TABLE);
     }
 }
