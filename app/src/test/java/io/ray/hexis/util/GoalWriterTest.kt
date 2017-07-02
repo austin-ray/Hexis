d11b0679b6abd0fc042569996ae395e4683158e5
Austin Ray <austinray1995@gmail.com>
2017-07-01 21:37:32 -0400

    Fix improper Gradle configuration
    Fixed an issue with the jacocoDebugReport and jacocoReleaseReport task
    configuration. Several critical lines were not in the previous commit.

---
+++ app/src/test/java/io/ray/hexis/util/GoalWriterTest.kt
@@ -0,0 +1,27 @@
+package io.ray.hexis.util
+
+import android.database.sqlite.SQLiteDatabase
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito
+
+class GoalWriterTest {
+
+  var writer: GoalWriter? = null
+
+  @Before
+  fun init() {
+    val mockHelper: SqlLiteHelper = Mockito.mock(SqlLiteHelper::class.java)
+    val mockDb: SQLiteDatabase = Mockito.mock(SQLiteDatabase::class.java)
+
+    Mockito.`when`(mockDb.insert(Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(1L)
+
+    writer = GoalWriter(mockHelper)
+  }
+
+  @Test
+  @Throws(Exception::class)
+  fun insertNewGoalTest() {
+    writer?.insertNewGoal("TEST")
+  }
+}
